6973326562fa3f15023085b1a8a7400f307f1ecf
Bami O. <bamideleogundeji949@gmail.com>
2024-10-09 11:01:15 -0400

    Update button color and text shadow in styles.css

---
+++ index.js
@@ -0,0 +1,5 @@
+//selecting the button and adding an event listener that listens for a click event
+document.querySelector("button").addEventListener("click", function(){
+    alert("Button clicked");
+});
+
